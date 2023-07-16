1d00be30481079bb2464f89f064c28fc6770e85c
Tushar <Tushar.debnath@revolut.com>
2023-07-16 09:09:52 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -13,10 +13,10 @@ export default function Home() {
           </nav>
         </header>
       </section>
-      <section className=" p-12 mt-12 max-w-5xl    border-2 border-purple-50 rounded-3xl  mx-auto flex space-x-12 items-center">
+      <section className=" p-12 mt-12 max-w-5xl     border-2 border-purple-50 rounded-3xl  mx-auto flex flex-col md:flex-row space-x-12 items-center">
         <img src="./shweta.jpeg" className="w-[220px] rounded-full border-8 border-purple-200 " />
         <div>
-          <h1 className="text-xl font-medium ">ABOUT ME</h1>
+          <h1 className="text-xl font-medium mt-4 md:mt-0 ">ABOUT ME</h1>
           <p>
             As a student of UX design, I embody a passionate and creative
             spirit, constantly seeking new avenues to blend logic and creativity
@@ -24,7 +24,6 @@ export default function Home() {
             strive for purity in my design approach. A deep appreciation for
             aesthetics drives my expression through dance, while my love for
             capturing meaningful moments adds depth to my creative journey.
-            {" "}
           </p>
         </div>
       </section>
